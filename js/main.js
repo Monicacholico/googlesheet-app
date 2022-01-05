56946f0243730194234ae01e9b3afd9147e4c2dc
Monicacholico <monicachol@hotmail.com>
2022-01-04 19:06:29 -0800

    added masonry

--- js/main.js
+++ js/main.js
@@ -183,3 +183,18 @@ function onTabClick(e){
 
 const element = document.getElementById('nav-tab');
 element.addEventListener('click', onTabClick, false);
+
+
+// MASONRY 
+
+var grid = document.querySelector('.grid');
+var msnry;
+
+imagesLoaded( grid, function() {
+  // init Isotope after all images have loaded
+  msnry = new Masonry( grid, {
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true
+  });
+});
